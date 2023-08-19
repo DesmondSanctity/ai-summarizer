e8a5576f9a7e2be131978b808397704d6b3db3ee
Desmond Obisi <desmondcutest@yahoo.com>
2023-08-19 23:19:58 +0100

    chore: added the  upload component in the UI

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,6 +6,18 @@ import { Editor } from '@tinymce/tinymce-react';
 export default function Home() {
   const editorRef = useRef(null);
   const [view, setView] = useState();
+
+  const [file, setFile] = useState()
+
+  function handleChange(e) {
+    setFile(e.target.files[0])
+  }
+
+  function handleReset() {
+    setFile();
+    setView();
+  }
+
   const log = () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
@@ -51,7 +63,7 @@ export default function Home() {
 
           <button
             className="px-4 py-2 bg-blue-500 text-white rounded ml-4"
-            onClick={() => setView()}
+            onClick={() => handleReset() }
           >
             Reset
           </button>
@@ -62,31 +74,78 @@ export default function Home() {
       <div className="flex flex-1 p-4">
 
         {/* Text Area */}
-        <div className="flex-1 pr-4">
-          <Editor
-            apiKey="skb5gw2zo2dgmtijsoi6e1pkjhainaet1bdsmml1iom4y2t1"
-            onInit={(evt, editor) => editorRef.current = editor}
-            initialValue="This is the initial content of the editor."
-            init={{
-              height: 600,
-              width: 1000,
-              menubar: false,
-              plugins: [
-                'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'export',
-                'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
-                'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
-              ],
-              toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
-                'alignleft aligncenter alignright alignjustify | ' +
-                'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
-              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-            }}
-          />
-          <button onClick={log} className="px-4 py-2 mt-4 bg-blue-500 text-white rounded">Summarize</button>
-        </div>
+        {view && view == "editor" && (
+          <div className="flex-1 pr-4">
+            <Editor
+              apiKey="skb5gw2zo2dgmtijsoi6e1pkjhainaet1bdsmml1iom4y2t1"
+              onInit={(evt, editor) => editorRef.current = editor}
+              init={{
+                height: 420,
+                width: 700,
+                menubar: false,
+                plugins: [
+                  'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                  'anchor', 'searchreplace', 'visualblocks', 'fullscreen',
+                  'insertdatetime', 'media', 'table', 'help', 'wordcount'
+                ],
+                toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
+                  'alignleft aligncenter alignright alignjustify | ' +
+                  'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
+                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+              }}
+            />
+            <button onClick={log} className="px-4 py-2 mt-4 bg-blue-500 text-white rounded">Summarize</button>
+          </div>
+        )}
+
+        {/* Upload File Area */}
+        {view === "upload" && (
+          <div className="flex-1 pr-4">
+            <div className="flex items-center justify-center">
+              <div className="border border-gray-400 bg-gray p-4 rounded shadow-md max-w-lg w-full">
+
+                <div className="flex flex-col items-center justify-center h-64">
+                  <input
+                    type="file"
+                    onChange={handleChange}
+                    className="border border-gray-400 rounded p-2 w-full mb-4"
+                  />
+
+                  {file && (
+                    <div className="text-center text-blue-400">
+                      <p className="text-lg font-medium">{file.name}</p>
+                      <p>{(file.size / (1024 * 1024)).toFixed(2)} MB</p>
+                    </div>
+                  )}
+                </div>
+
+                <button className="bg-blue-500 text-white px-4 py-2 rounded w-full">
+                  Summarize
+                </button>
+
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* Default Screen Area */}
+        {!view && (
+          <div className="flex-1 pr-4">
+            <div className="flex items-center justify-center h-full">
+              <div className="text-center">
+                <h2 className="text-2xl font-bold mb-4">
+                  Welcome to SummarizeAI
+                </h2>
+                <p className="text-gray-500 max-w-md">
+                  Please select an input format above to get started
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
 
         {/* Side Section */}
-        <div className="flex-1 border border-gray-500 rounded p-4" style={{ height: '70vh' }}>
+        <div className="flex-1 border border-gray-400 rounded p-4" style={{ height: '70vh' }}>
           <p>Summarized content here...</p>
         </div>
 
